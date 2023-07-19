163291b68af3030927f8146747a9738e3f532f7e
Volodymyr Bondar <2005volka@gmail.com>
2023-07-19 17:44:21 +0300

    template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
+import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './contactForm/ContactForm';
+import ContactList from './contactList/ContactList';
+import Filter from './filter/Filter';
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact, deleteContact } from '../redux/contactsSlice';
+import { setFilter } from '../redux/filterSlice';
+
+export function App() {
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (name, number) => {
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+
+    dispatch(addContact(newContact));
+  };
+
+  const handleFilterChange = event => {
+    dispatch(setFilter(event.target.value));
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ marginLeft: 40 }}>
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} handleChange={handleFilterChange} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        handleDeleteContact={handleDeleteContact}
+      />
     </div>
   );
-};
+}
